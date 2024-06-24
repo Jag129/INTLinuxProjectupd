047c760ac0f1921490bfaa12ddad9697a6763dad
jess <yisrael129@gmail.com>
2024-06-24 22:07:09 +0300

    test 1000

--- yourSolution.sh
+++ yourSolution.sh
@@ -1,8 +1,8 @@
-cd '/home/jess/Downloads/project/'
+cd ~/Downloads/project/
 pwd
 wget https://alonitac.github.io/DevOpsTheHardWay/linux_project/secretGenerator.tar.gz
 tar -xvzf secretGenerator.tar.gz
-cd '/home/jess/Downloads/project/src'
+cd ~/Downloads/project/src
 mkdir secretDir
 #mv -f src/* ./
 #rm -rf src
@@ -10,9 +10,9 @@ touch secretDir/.secret
 chmod 600 secretDir/.secret
 chmod a-x secretDir/.secret
 rm -rf ~/Downloads/project/src/maliciousFiles
-chmod +x '/home/jess/Downloads/project/src/generateSecret.sh'
+chmod +x ~/Downloads/project/src/generateSecret.sh
 rm -rf important.link
-bash '/home/jess/Downloads/project/src/generateSecret.sh'
+bash ~/Downloads/project/src/generateSecret.sh
 
 
 

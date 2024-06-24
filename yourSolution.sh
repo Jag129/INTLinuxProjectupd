a77e31c97efffbc8f10bea35974f95bd23e8b9b9
jess <yisrael129@gmail.com>
2024-06-24 15:41:57 +0300

    Semi-Finala

--- yourSolution.sh
+++ yourSolution.sh
@@ -1,14 +1,15 @@
-mkdir -p ~/Downloads/project/src/secretDir
-pwd
-cd ~/Downloads/project/src/
+cd ~/Downloads/project/
 pwd
 wget https://alonitac.github.io/DevOpsTheHardWay/linux_project/secretGenerator.tar.gz
 tar -xvzf secretGenerator.tar.gz
+cd ~/Downloads/project/src
+mkdir secretDir
 #mv -f src/* ./
 #rm -rf src
 touch secretDir/.secret
 chmod 600 secretDir/.secret
 chmod a-x secretDir/.secret
+rm -rf ~/Downloads/project/src/maliciousFiles
 chmod +x ~/Downloads/project/src/generateSecret.sh
 rm -rf important.link
 /bin/bash ~/Downloads/project/src/generateSecret.sh
